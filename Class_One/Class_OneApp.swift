f8283dd09175ba2994072448d130366568047c66
Nearezz <biswasindranil019@gmail.com>
2024-08-18 21:09:10 -0400

    Initial Commit

---
+++ Class_One/Class_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Class_OneApp.swift
+//  Class_One
+//
+//  Created by biswas on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Class_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
